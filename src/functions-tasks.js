949ace9b6723616878c59558c67c3e234be6b53b
Dmitry Ishchenko <19191458+DmitriyIshchenko@users.noreply.github.com>
2025-03-24 21:16:15 +0300

    feat: implement getPolynom

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -88,8 +88,15 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  return function (x) {
+    if (!args.length) return null;
+
+    return [...args]
+      .reverse()
+      .map((coef, index) => Math.pow(x, index) * coef)
+      .reduce((acc, cur) => acc + cur, 0);
+  };
 }
 
 /**
