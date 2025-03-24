b416ea96c5890c48d59d928085ac4a5fcfb31168
Dmitry Ishchenko <19191458+DmitriyIshchenko@users.noreply.github.com>
2025-03-24 21:24:40 +0300

    feat: implement id generator

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -215,10 +215,10 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom;
+  return () => id++;
 }
-
 module.exports = {
   getCurrentFunctionName,
   getFunctionBody,
